7e4d26ce7dc74c125f795d96d4f8549af5be9ce4
Osama <iosamaify@gmail.com>
2021-04-19 15:33:47 +0200

    arguments parsing

--- baseconv.c
+++ baseconv.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAME "baseconv"
-#define eprintf(msg, ...) fprintf(stderr, NAME": "msg, __VA_ARGS__)
-#define eprintln(msg) fprintf(stderr, NAME": "msg"\n")
+#define NAME "nbc"
+#define panic(...) { fprintf(stderr, __VA_ARGS__); exit(1); }
 
 enum numtype {
 	NT_UNKNOWN,
@@ -29,28 +28,35 @@ enum numtype numtype_parse(char *s) {
 
 int main(int argc, char *argv[]) {
 	if (argc == 1) {
-		eprintln("at least one argument is required");
-		exit(EXIT_FAILURE);
+		panic(NAME": at least one argument is required\n");
 	}
 
 	enum numtype fromtype = NT_UNKNOWN;
 	enum numtype totype = NT_UNKNOWN;
 
+	char *argp = NULL;
 	for (int i = 0; i < argc; i++) {
-		char *s = argv[i];
-		if (argv[i][0] == '-') {
-			s++;
-			// skip second dash
-			if (argv[i][1] == '-') s++;
-			if (strcmp(s, "from") == 0) {
+		argp = argv[i];
+		if (argp[0] == '-') {
+			argp++;
+			if (argp[0] == '-') argp++;
+			if (strcmp(argp, "from") == 0) {
+				if (argc <= i+1 || argv[i+1][0] == '-') {
+					panic(NAME": %s expect a value\n", argv[i]);
+				}
 				fromtype = numtype_parse(argv[i+1]);
 				i++;
-			} else if (strcmp(s, "to") == 0) {
+			} else if (strcmp(argp, "to") == 0) {
+				if (argc <= i+1 || argv[i+1][0] == '-') {
+					panic(NAME": %s expect a value\n", argv[i]);
+				}
 				totype = numtype_parse(argv[i+1]);
 				i++;
+			} else {
+				panic(NAME": unknown argument %s\n", argv[i]);
 			}
 		}
 	}
 
-	return EXIT_SUCCESS;
+	return 0;
 }
