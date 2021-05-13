c6e8564ef3002b8df5a5fe5d5864a1dc9675b84d
Osama <iosamaify@gmail.com>
2021-05-13 13:40:03 +0200

    stuff

--- baseconv.c
+++ baseconv.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NAME "nbc"
+#ifndef
+#define NAME "baseconv"
+#endif
+
 #define panic(...) { fprintf(stderr, __VA_ARGS__); exit(1); }
 
-enum numtype {
-	NT_UNKNOWN,
-	NT_BINARY,
-	NT_OCTAL,
-	NT_DECIMAL,
-	NT_HEX,
-};
+typedef enum numtype {
+	NT_UNKNOWN = 0,
+	NT_BINARY = 2,
+	NT_OCTAL = 8,
+	NT_DECIMAL = 10,
+	NT_HEX = 16,
+} numtype_t;
 
-enum numtype numtype_parse(char *s) {
+numtype_t parse_numtype(char *s) {
 	if (strcmp(s, "binary")) {
 		return NT_BINARY;
 	} else if (strcmp(s, "octal") == 0) {
@@ -31,30 +34,33 @@ int main(int argc, char *argv[]) {
 		panic(NAME": at least one argument is required\n");
 	}
 
-	enum numtype fromtype = NT_UNKNOWN;
-	enum numtype totype = NT_UNKNOWN;
+	numtype_t optfrom = NT_UNKNOWN;
+	numtype_t optto = NT_UNKNOWN;
 
 	char *argp = NULL;
+	char *input = NULL;
 	for (int i = 0; i < argc; i++) {
 		argp = argv[i];
-		if (argp[0] == '-') {
-			argp++;
-			if (argp[0] == '-') argp++;
-			if (strcmp(argp, "from") == 0) {
-				if (argc <= i+1 || argv[i+1][0] == '-') {
-					panic(NAME": %s expect a value\n", argv[i]);
-				}
-				fromtype = numtype_parse(argv[i+1]);
-				i++;
-			} else if (strcmp(argp, "to") == 0) {
-				if (argc <= i+1 || argv[i+1][0] == '-') {
-					panic(NAME": %s expect a value\n", argv[i]);
-				}
-				totype = numtype_parse(argv[i+1]);
-				i++;
-			} else {
-				panic(NAME": unknown argument %s\n", argv[i]);
+		if (argp[0] != '-') {
+			input = argp;
+			continue;
+		}
+		argp++;
+		if (argp[0] == '-') argp++;
+		if (strcmp(argp, "from") == 0) {
+			if (argc <= i+1 || argv[i+1][0] == '-') {
+				panic(NAME": %s expect a value\n", argv[i]);
 			}
+			optfrom = numtype_parse(argv[i+1]);
+			i++;
+		} else if (strcmp(argp, "to") == 0) {
+			if (argc <= i+1 || argv[i+1][0] == '-') {
+				panic(NAME": %s expect a value\n", argv[i]);
+			}
+			optto = numtype_parse(argv[i+1]);
+			i++;
+		} else {
+			panic(NAME": unknown argument %s\n", argv[i]);
 		}
 	}
 
