5c056fd0399d1431b6b70defe91aef25195df71d
Osama <iosamaify@gmail.com>
2021-05-14 00:54:28 +0200

    octal

--- baseconv.c
+++ baseconv.c
@@ -18,13 +18,13 @@ typedef enum numtype {
 } numtype_t;
 
 numtype_t parse_numtype(char *s) {
-	if (strcmp(s, "binary")) {
+	if (strcmp(s, "bin") == 0 || strcmp(s, "binary") == 0) {
 		return NT_BINARY;
 	} else if (strcmp(s, "octal") == 0) {
 		return NT_OCTAL;
 	} else if (strcmp(s, "decimal") == 0) {
 		return NT_DECIMAL;
-	} else if (strcmp(s, "hex") == 0) {
+	} else if (strcmp(s, "hex") == 0 || strcmp(s, "hexadecimal") == 0) {
 		return NT_HEX;
 	}
 	return NT_UNKNOWN;
@@ -33,7 +33,16 @@ numtype_t parse_numtype(char *s) {
 bool isbinary(char *input) {
 	for (size_t i = 0; i < strlen(input); i++) {
 		if (input[i] != '0' && input[i] != '1') {
-			return false
+			return false;
+		}
+	}
+	return true;
+}
+
+bool isoctal(char *input) {
+	for (size_t i = 0; i < strlen(input); i++) {
+		if (input[i] == '8' || input[i] == '9') {
+			return false;
 		}
 	}
 	return true;
@@ -74,18 +83,46 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	if (!input) {
+	if (!input || input[0] == '\0') {
 		panic(NAME": empty input\n");
+	}
+
+	if (optfrom == NT_BINARY && !isbinary(input)) {
+		panic(NAME": input: invalid binary format\n");
 	}
 
 	if (optfrom == NT_UNKNOWN) {
 		if (input[0] == 'x' || (input[0] == '0' && (input[1] == 'x' || input[1] == 'X'))) {
 			optfrom = NT_HEX;
 		} else if (input[0] == '0') {
-			optfrom = isbinary(input) ? NT_BINARY : NT_OCTAL;
+			if (isbinary(input)) {
+				optfrom = NT_BINARY;
+			} else if (isoctal(input)) {
+				optfrom = NT_OCTAL;
+			} else {
+				optfrom = NT_DECIMAL;
+				input++;
+			}
 		} else {
 			optfrom = isbinary(input) ? NT_BINARY : NT_DECIMAL;
 		}
+	}
+
+	long num = strtol(input, 0, optfrom);
+	printf("%d\t%d\n%ld\n", optfrom, optto, num);
+
+	switch (optto) {
+		case NT_BINARY:
+			break;
+		case NT_OCTAL:
+			printf("%o\n", (unsigned int) num);
+			break;
+		case NT_HEX:
+			printf("%x\n", (unsigned int) num);
+			break;
+		default: // NT_DECIMAL, NT_UNKNOWN
+			printf("%ld\n", num);
+			break;
 	}
 
 	return 0;
